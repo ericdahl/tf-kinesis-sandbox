68cec91c6ed6c8c0cca51dbeb684979e4be18ebc
Eric Dahl <eric.c.dahl@gmail.com>
2022-05-07 12:34:44 -0700

    set a partition key

---
+++ README.md
@@ -0,0 +1,9 @@
+# tf-kinesis-sandbox
+
+Example apps using Kinesis Data Streams and Firehose
+
+## TODO
+
+- Data Streams
+  - Exceed shard limits
+- Firehose - Lambda processing example
--- lambda/producer/main.py
+++ lambda/producer/main.py
@@ -3,6 +3,7 @@ import json
 import boto3
 import os
 from datetime import datetime
+import random
 
 import logging
 logging.getLogger().setLevel(logging.INFO)
@@ -16,5 +17,5 @@ def handler(event, context):
     client.put_record(
         StreamName=os.environ['KINESIS_STREAM_NAME'],
         Data=os.environ["LAMBDA_ID"] + "/" + datetime.now().isoformat() + "\n",
-        PartitionKey='123'  # FIXME
+        PartitionKey=random.randrange(100)
     )
