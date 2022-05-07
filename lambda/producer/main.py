12ca35690f4a05112cdc57bcfc672ea50d5ca201
Eric Dahl <eric.c.dahl@gmail.com>
2022-05-07 11:20:04 -0700

    fix syntax error

--- lambda/producer/main.py
+++ lambda/producer/main.py
@@ -4,16 +4,21 @@ import boto3
 from datetime import datetime
 import os
 
+import logging
+logging.getLogger().setLevel(logging.INFO)
+
 client = boto3.client('kinesis')
 
 
 def handler(event, context):
     print("Received event: " + json.dumps(event, indent=2))
 
-
     client.put_record(
-        StreamName=os.environ['KINESIS_STREAM_NAME']
+        StreamName=os.environ['KINESIS_STREAM_NAME'],
         Data=datetime.now().isoformat(),
         PartitionKey='123' # FIXME
     )
 
+
+if __name__ == '__main__':
+    print("foo")
